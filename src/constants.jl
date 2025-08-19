2548a11e572074315092376c7aab2788829061b2
Iain Best <iain.best@hotmail.com>
2025-08-19 12:29:50 +0100

    add constants file

---
+++ src/constants.jl
@@ -0,0 +1,10 @@
+const EMPTY = 0
+const DWARF = 1
+const TROLL = 2
+const KING = 3
+const OUTSIDE = 4
+
+const BOARD_SIZE = 15
+
+const files = ["A","B","C","D","E","F","G","H","J","K","L","M","N","O","P"]
+const ranks = string.([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
