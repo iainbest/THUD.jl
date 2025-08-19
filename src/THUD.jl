0ccc6791f8e53a1b0a72bef6c48fa7fed6b7d24d
Iain Best <iain.best@hotmail.com>
2025-08-19 12:31:23 +0100

    add THUD.jl module file

---
+++ src/THUD.jl
@@ -0,0 +1,14 @@
+module THUD
+
+using Revise
+using LinearAlgebra
+using Plots
+
+### includet for revise.jl revisions
+includet("./constants.jl")
+includet("./extrafunctions.jl")
+includet("./evaluation.jl")
+includet("./printing.jl")
+includet("./minimax.jl")
+
+end
