76f965665b729aa06eef52ba8ab6f2c832ebe69c
Iain Best <iain.best@hotmail.com>
2025-10-04 19:24:28 +0100

    add random and staticarrays using statements

--- src/THUD.jl
+++ src/THUD.jl
@@ -1,7 +1,9 @@
 module THUD
 using Reexport
 @reexport using Revise
+@reexport using Random
 @reexport using LinearAlgebra
+@reexport using StaticArrays
 # @reexport using Plots
 @reexport using GLMakie
 @reexport using JLD2
@@ -13,8 +15,8 @@ includet("constants.jl")
 includet("extrafunctions.jl")
 includet("evaluation.jl")
 includet("printing.jl")
-includet("strings.jl")
 includet("movement.jl")
+includet("strings.jl")
 includet("engines.jl")
 
 end
